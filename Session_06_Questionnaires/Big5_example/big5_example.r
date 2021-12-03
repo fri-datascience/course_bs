3bf57ec71ec2756cd3dfe97dc2df279312510915
Erik Štrumbelj <strumbloid@gmail.com>
2021-12-04 00:11:17 +0100

    Minor changes to Questionnaires

--- Session_06_Questionnaires/Big5_example/big5_example.r
+++ Session_06_Questionnaires/Big5_example/big5_example.r
@@ -1,3 +1,56 @@
+# The scale was labeled 1=Disagree, 3=Neutral, 5=Agree
+# 
+# EXT1	I am the life of the party.
+# EXT2	I don't talk a lot.
+# EXT3	I feel comfortable around people.
+# EXT4	I keep in the background.
+# EXT5	I start conversations.
+# EXT6	I have little to say.
+# EXT7	I talk to a lot of different people at parties.
+# EXT8	I don't like to draw attention to myself.
+# EXT9	I don't mind being the center of attention.
+# EXT10	I am quiet around strangers.
+# EST1	I get stressed out easily.
+# EST2	I am relaxed most of the time.
+# EST3	I worry about things.
+# EST4	I seldom feel blue.
+# EST5	I am easily disturbed.
+# EST6	I get upset easily.
+# EST7	I change my mood a lot.
+# EST8	I have frequent mood swings.
+# EST9	I get irritated easily.
+# EST10	I often feel blue.
+# AGR1	I feel little concern for others.
+# AGR2	I am interested in people.
+# AGR3	I insult people.
+# AGR4	I sympathize with others' feelings.
+# AGR5	I am not interested in other people's problems.
+# AGR6	I have a soft heart.
+# AGR7	I am not really interested in others.
+# AGR8	I take time out for others.
+# AGR9	I feel others' emotions.
+# AGR10	I make people feel at ease.
+# CSN1	I am always prepared.
+# CSN2	I leave my belongings around.
+# CSN3	I pay attention to details.
+# CSN4	I make a mess of things.
+# CSN5	I get chores done right away.
+# CSN6	I often forget to put things back in their proper place.
+# CSN7	I like order.
+# CSN8	I shirk my duties.
+# CSN9	I follow a schedule.
+# CSN10	I am exacting in my work.
+# OPN1	I have a rich vocabulary.
+# OPN2	I have difficulty understanding abstract ideas.
+# OPN3	I have a vivid imagination.
+# OPN4	I am not interested in abstract ideas.
+# OPN5	I have excellent ideas.
+# OPN6	I do not have a good imagination.
+# OPN7	I am quick to understand things.
+# OPN8	I use difficult words.
+# OPN9	I spend time reflecting on things.
+# OPN10	I am full of ideas.
+
 library(psych)
 library(GPArotation)
 
