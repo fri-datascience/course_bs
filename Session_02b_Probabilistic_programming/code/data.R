5da7e4777ecdf645a960735b02c6720d43ac6109
Jure Demsar <demsarjure@gmail.com>
2022-10-11 18:09:47 +0200

    Removed an unused code file.

--- Session_02b_Probabilistic_programming/code/data.R
+++
@@ -1,9 +0,0 @@
-# coin flip data ---------------------------------------------------------------
-# sequence
-y <- c(1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0)
-
-# number of flips
-n <- length(y)
-
-# number of heads
-z <- sum(y)
