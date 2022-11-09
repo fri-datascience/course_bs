837052c9f226166114c48ccac9e9df2579c7a398
Jure Demsar <demsarjure@gmail.com>
2022-11-09 12:51:09 +0100

    Code style consistent with other lectures.

--- Session_04_Describe_the_process/code/01_golfing.R
+++ Session_04_Describe_the_process/code/01_golfing.R
@@ -1,21 +1,20 @@
 # libraries --------------------------------------------------------------------
-library(cmdstanr)   # for interfacing Stan
-library(ggplot2)    # for visualizations
-library(posterior)  # for extracting samples
-library(bayesplot)  # for some quick MCMC visualizations
-library(mcmcse)     # for comparing samples and calculating MCSE
-library(arm)        # for logit and inverse logit functions
-library(tidyverse)  # for data manipulations
-library(HDInterval) # for HDI
+library(cmdstanr)
+library(ggplot2)
+library(posterior)
+library(bayesplot)
+library(mcmcse)
+library(arm)
+library(tidyverse)
+library(HDInterval)
 
 # color palette ----------------------------------------------------------------
 # 1 - #a6cee3
 # 2 - #1f78b4
 # 3 - #b2df8a
 
-
 # data prep --------------------------------------------------------------------
-data <- read.csv("../data/golf.csv", sep="\t")
+data <- read.csv("../data/golf.csv", sep = "\t")
 
 # input data to stan will be always the same
 stan_data <- list(N = nrow(data),
@@ -25,34 +24,39 @@ stan_data <- list(N = nrow(data),
 
 # distances sequence used later on for visualizations
 precision <- 100
-x <- seq(from=0, to=max(data$distance), length.out=precision)
+x <- seq(from = 0, to = max(data$distance), length.out = precision)
 
 # calculate error bars in data for visualizations
-df_golf <- data.frame(x=numeric(), p=numeric(), hdi5=numeric(), hdi95=numeric())
-for (i in 1:nrow(data)) {
+df_golf <- data.frame(x = numeric(),
+                      p = numeric(),
+                      hdi5 = numeric(),
+                      hdi95 = numeric())
+for (i in seq_len(nrow(data))) {
   # get row
-  row <- data[i,]
-  
+  row <- data[i, ]
+
   # calculate percentage
   p <- row$y / row$n
-  
+
   # draw
   b <- rbinom(1000, row$n, p) / row$n
-  
+
   # hdi
-  hdi90 <- hdi(b, credMass=0.90)
-  
+  hdi90 <- hdi(b, credMass = 0.90)
+
   # bind
-  df_golf <- df_golf %>% add_row(x=row$distance, p=p, hdi5=hdi90[1], hdi95=hdi90[2])
+  df_golf <- df_golf %>% add_row(x = row$distance,
+                                 p = p,
+                                 hdi5 = hdi90[1],
+                                 hdi95 = hdi90[2])
 }
 
 # plot data
-ggplot(df_golf, aes(x=x, y=p, ymin=hdi5, ymax=hdi95)) +
-  geom_errorbar(color="grey75") +
-  geom_point(color="grey25") +
+ggplot(df_golf, aes(x = x, y = p, ymin = hdi5, ymax = hdi95)) +
+  geom_errorbar(color = "grey75") +
+  geom_point(color = "grey25") +
   theme_minimal() +
   ylim(0, 1)
-
 
 # binomial logit modelling -----------------------------------------------------
 # compile the model
@@ -81,48 +85,50 @@ mcse(df_binomial_samples$b)
 X <- matrix(0, nrow = nrow(df_binomial_samples), ncol = precision)
 
 # calculate probabilities from samples
-for (i in 1:nrow(df_binomial_samples)) {
+for (i in seq_len(nrow(df_binomial_samples))) {
   # extract values
-  a <- df_binomial_samples[i,]$a
-  b <- df_binomial_samples[i,]$b
+  a <- df_binomial_samples[i, ]$a
+  b <- df_binomial_samples[i, ]$b
 
   # calculate probability
-  X[i,] <- invlogit(a + b * x)
+  X[i, ] <- invlogit(a + b * x)
 }
 
 # data frame for storing probabilities
-df_binomial <- data.frame(x=numeric(), p=numeric(), hdi5=numeric(), hdi95=numeric())
+df_binomial <- data.frame(x = numeric(),
+                          p = numeric(),
+                          hdi5 = numeric(),
+                          hdi95 = numeric())
 
 # calculate mean and hdi for each distance
-for (i in 1:ncol(X)) {
+for (i in seq_len(ncol(X))) {
   # get column
   column <- X[, i]
-  
+
   # calcuate
   p <- mean(column)
-  
+
   # hdi
-  hdi90 <- hdi(column, credMass=0.90)
-  
+  hdi90 <- hdi(column, credMass = 0.90)
+
   # bind
   df_binomial <- df_binomial %>%
-    add_row(x=x[i], p=p, hdi5=hdi90[1], hdi95=hdi90[2])
+    add_row(x = x[i], p = p, hdi5 = hdi90[1], hdi95 = hdi90[2])
 }
 
 # visualize data and results
 ggplot(df_binomial,
-       aes(x=x, y=p, ymin=hdi5, ymax=hdi95)) +
+       aes(x = x, y = p, ymin = hdi5, ymax = hdi95)) +
   geom_errorbar(df_golf,
-                mapping=aes(x=x, y=p, ymin=hdi5, ymax=hdi95),
-                color="grey75") +
+                mapping = aes(x = x, y = p, ymin = hdi5, ymax = hdi95),
+                color = "grey75") +
   geom_point(df_golf,
-             mapping=aes(x=x, y=p),
-             color="grey25") +
-  geom_ribbon(fill="#a6cee3", alpha=0.5) +
-  geom_line(color="#a6cee3") +
+             mapping = aes(x = x, y = p),
+             color = "grey25") +
+  geom_ribbon(fill = "#a6cee3", alpha = 0.5) +
+  geom_line(color = "#a6cee3") +
   theme_minimal() +
   ylim(0, 1)
-
 
 # modelling error in the angle -------------------------------------------------
 # compile the model
@@ -149,46 +155,48 @@ mcse(df_angle_samples$sigma_degrees)
 
 # extract probabilities
 X <- as_draws_matrix(fit_angle$draws())
-X <- X[,4:(4+nrow(data)-1)]
+X <- X[, 4:(4 + nrow(data) - 1)]
 
 # data frame for storing probabilities
-df_angle <- data.frame(x=numeric(), p=numeric(), hdi5=numeric(), hdi95=numeric())
+df_angle <- data.frame(x = numeric(),
+                       p = numeric(),
+                       hdi5 = numeric(),
+                       hdi95 = numeric())
 
 # calculate mean and hdi for each distance
-for (i in 1:ncol(X)) {
+for (i in seq_len(ncol(X))) {
   # get column
   column <- X[, i]
-  
+
   # calcuate
   p <- mean(column)
-  hdi90 <- hdi(column, credMass=0.90)
-    
+  hdi90 <- hdi(column, credMass = 0.90)
+
   # bind
   df_angle <- df_angle %>%
-    add_row(x=data$distance[i], p=p, hdi5=hdi90[1], hdi95=hdi90[2])
+    add_row(x = data$distance[i], p = p, hdi5 = hdi90[1], hdi95 = hdi90[2])
 }
 
 # visualize data and results
 ggplot(df_angle,
-       aes(x=x, y=p, ymin=hdi5, ymax=hdi95)) +
+       aes(x = x, y = p, ymin = hdi5, ymax = hdi95)) +
   geom_errorbar(df_golf,
-                mapping=aes(x=x, y=p, ymin=hdi5, ymax=hdi95),
-                color="grey75") +
+                mapping = aes(x = x, y = p, ymin = hdi5, ymax = hdi95),
+                color = "grey75") +
   geom_point(df_golf,
-             mapping=aes(x=x, y=p), 
-             color="grey25") +
+             mapping = aes(x = x, y = p),
+             color = "grey25") +
   geom_ribbon(df_binomial,
-              mapping=aes(x=x, y=p, ymin=hdi5, ymax=hdi95),
-              fill="#a6cee3",
-              alpha=0.5) +
+              mapping = aes(x = x, y = p, ymin = hdi5, ymax = hdi95),
+              fill = "#a6cee3",
+              alpha = 0.5) +
   geom_line(df_binomial,
-            mapping=aes(x=x, y=p),
-            color="#a6cee3") +
-  geom_ribbon(fill="#1f78b4", alpha=0.5) +
-  geom_line(color="#1f78b4") +
+            mapping = aes(x = x, y = p),
+            color = "#a6cee3") +
+  geom_ribbon(fill = "#1f78b4", alpha = 0.5) +
+  geom_line(color = "#1f78b4") +
   theme_minimal() +
   ylim(0, 1)
-
 
 # modelling error in the angle and in the distance -----------------------------
 # compile the model
@@ -216,48 +224,50 @@ mcse(df_distance_samples$sigma_d)
 
 # extract probabilities
 X <- as_draws_matrix(fit_distance$draws())
-X <- X[,5:(5+nrow(data)-1)]
+X <- X[, 5:(5 + nrow(data) - 1)]
 
 # data frame for storing probabilities
-df_distance <- data.frame(x=numeric(), p=numeric(), hdi5=numeric(), hdi95=numeric())
+df_distance <- data.frame(x = numeric(),
+               p = numeric(), hdi5 = numeric(), hdi95 = numeric())
 
 # calculate mean and hdi for each distance
-for (i in 1:ncol(X)) {
+for (i in seq_len(ncol(X))) {
   # get column
   column <- X[, i]
-  
+
   # calcuate
   p <- mean(column)
-  hdi90 <- hdi(column, credMass=0.90)
-    
+  hdi90 <- hdi(column, credMass = 0.90)
+
   # bind
   df_distance <- df_distance %>%
-    add_row(x=data$distance[i], p=p, hdi5=hdi90[1], hdi95=hdi90[2])
+    add_row(x = data$distance[i], p = p, hdi5 = hdi90[1], hdi95 = hdi90[2])
 }
+
 # visualize data and results
 ggplot(df_distance,
-       aes(x=x, y=p, ymin=hdi5, ymax=hdi95)) +
+       aes(x = x, y = p, ymin = hdi5, ymax = hdi95)) +
   geom_errorbar(df_golf,
-                mapping=aes(x=x, y=p, ymin=hdi5, ymax=hdi95),
-                color="grey75") +
+                mapping = aes(x = x, y = p, ymin = hdi5, ymax = hdi95),
+                color = "grey75") +
   geom_point(df_golf,
-             mapping=aes(x=x, y=p), 
-             color="grey25") +
+             mapping = aes(x = x, y = p),
+             color = "grey25") +
   geom_ribbon(df_binomial,
-              mapping=aes(x=x, y=p, ymin=hdi5, ymax=hdi95),
-              fill="#a6cee3",
-              alpha=0.5) +
+              mapping = aes(x = x, y = p, ymin = hdi5, ymax = hdi95),
+              fill = "#a6cee3",
+              alpha = 0.5) +
   geom_line(df_binomial,
-            mapping=aes(x=x, y=p),
-            color="#a6cee3") +
+            mapping = aes(x = x, y = p),
+            color = "#a6cee3") +
   geom_ribbon(df_angle,
-              mapping=aes(x=x, y=p, ymin=hdi5, ymax=hdi95),
-              fill="#1f78b4",
-              alpha=0.5) +
+              mapping = aes(x = x, y = p, ymin = hdi5, ymax = hdi95),
+              fill = "#1f78b4",
+              alpha = 0.5) +
   geom_line(df_angle,
-            mapping=aes(x=x, y=p),
-            color="#1f78b4") +
-  geom_ribbon(fill="#b2df8a", alpha=0.5) +
-  geom_line(color="#b2df8a") +
+            mapping = aes(x = x, y = p),
+            color = "#1f78b4") +
+  geom_ribbon(fill = "#b2df8a", alpha = 0.5) +
+  geom_line(color = "#b2df8a") +
   theme_minimal() +
   ylim(0, 1)
