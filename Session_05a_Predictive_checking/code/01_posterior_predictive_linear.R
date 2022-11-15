fc01ab2a05cca5b5af2a6cfd3eab77002dde1c00
Jure Demsar <demsarjure@gmail.com>
2022-11-15 09:56:37 +0100

    Code style tweaks.

--- Session_05a_Predictive_checking/code/01_posterior_predictive_linear.R
+++ Session_05a_Predictive_checking/code/01_posterior_predictive_linear.R
@@ -6,13 +6,12 @@ library(posterior)
 library(tidyverse)
 library(mcmcse)
 
-
 # modelling and data prep ------------------------------------------------------
 # compile the model
 model <- cmdstan_model("../models/simple_linear.stan")
 
 # load the data
-data <- read.csv("../data/temperature.csv", sep=";")
+data <- read.csv("../data/temperature.csv", sep = ";")
 
 # remove month
 data <- data %>% select(-month)
@@ -29,7 +28,7 @@ data$year0 <- data$year - min(data$year)
 n <- nrow(data)
 x <- data$year0
 y <- data$temperature
-stan_data <- list(n=n, x=x, y=y)
+stan_data <- list(n = n, x = x, y = y)
 
 # fit
 fit <- model$sample(
@@ -37,7 +36,6 @@ fit <- model$sample(
   parallel_chains = 4,
   seed = 1
 )
-
 
 # diagnostics ------------------------------------------------------------------
 # traceplot
@@ -54,22 +52,24 @@ df <- as_draws_df(fit$draws())
 mcse(df$b)
 
 # hdi
-hdi(df$b, credMass=0.90)
+hdi(df$b, credMass = 0.90)
 
 # visualize data points and regression lines
 # plot only 200 random regression lines
-df_100 <- data.frame(alpha=df$a, beta=df$b)
+df_100 <- data.frame(alpha = df$a, beta = df$b)
 df_100 <- sample_n(df_100, 100)
 
 # plot
-ggplot() + 
-  geom_point(data=data,
-             aes(x=year0, y=temperature),
-             alpha=0.3, size=1.5, shape=16) +
+ggplot() +
+  geom_point(data = data,
+             aes(x = year0, y = temperature),
+             alpha = 0.3, size = 1.5, shape = 16) +
   geom_abline(data = df_100,
               aes(slope = beta, intercept = alpha),
-              alpha=0.05, size=1) +
+              alpha = 0.05, size = 1) +
   ylim(6, 12) +
-  scale_x_continuous(breaks=c(0, 40, 80, 120), labels=c(1900, 1940, 1980, 2020), limits=c(0, 120)) +
+  scale_x_continuous(breaks = c(0, 40, 80, 120),
+                     labels = c(1900, 1940, 1980, 2020),
+                     limits = c(0, 120)) +
   xlab("Year") +
   ylab("T [°C]")
