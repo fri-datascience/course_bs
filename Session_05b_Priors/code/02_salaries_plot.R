b2c105a832b5ca2a34882b654369f8b57a9c44f5
Jure Demsar <demsarjure@gmail.com>
2022-11-16 11:11:32 +0100

    Added salaries plotter.

---
+++ Session_05b_Priors/code/02_salaries_plot.R
@@ -0,0 +1,34 @@
+# libraries --------------------------------------------------------------------
+library(ggplot2)
+library(tidyverse)
+
+# load the data ----------------------------------------------------------------
+df <- read.csv("../data/salaries.csv")
+
+# to thousands
+df$salary <- df$salary / 1000
+
+# gammas -----------------------------------------------------------------------
+gammas <- data.frame(
+  k = c(3.122, 6.517, 7.731),
+  t = c(0.641, 0.862, 1.552)
+)
+
+# calculate density
+x_max <- 15
+x <- seq(0, x_max, length.out = 1000)
+
+df_gamma <- data.frame(x = numeric(), y = numeric(), group = factor())
+
+for (i in seq_len(nrow(gammas))) {
+  gamma <- gammas[i, ]
+  y <- dgamma(x, gamma$k, scale = gamma$t)
+  df_gamma <- df_gamma %>% add_row(x = x, y = y, group = as.factor(i))
+}
+
+# plot -------------------------------------------------------------------------
+ggplot(data = df, aes(x = salary)) +
+  geom_density(color = NA, fill = "skyblue", alpha = 0.5) +
+  geom_line(data = df_gamma, aes(x = x, y = y, color = group), size = 2) +
+  xlim(0, x_max) +
+  scale_color_brewer(type = "qual", palette = 2)
