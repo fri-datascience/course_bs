fd1153f3ecbf6da7a9a391d802a6d4d00230b24f
Jure Demsar <demsarjure@gmail.com>
2024-10-10 08:02:35 +0200

    Updated uncertainty terms with 2024 data.

--- Session_02_Probabilistic_thinking/code/02_expressions_google.R
+++ Session_02_Probabilistic_thinking/code/02_expressions_google.R
@@ -4,7 +4,7 @@ library(ggridges)
 library(forcats)
 
 # load the data
-df <- read.csv("../data/expressions2023.csv")
+df <- read.csv("../data/expressions2024.csv")
 df <- df %>% select(-Timestamp)
 
 # to long format
@@ -14,18 +14,18 @@ df_long$value <- ifelse(df_long$value < 1, df_long$value * 100, df_long$value)
 
 # sort descendingly by average value
 average_values <- df_long %>%
-    group_by(term) %>%
-    summarize(average_value = mean(value)) %>%
-    arrange(average_value)
+  group_by(term) %>%
+  summarize(average_value = mean(value)) %>%
+  arrange(average_value)
 df_long$term <- factor(df_long$term, levels = average_values$term)
 
 # plot
 ggplot(df_long, aes(x = value, y = term, fill = term)) +
-    geom_density_ridges(fill = "skyblue", jittered_points = TRUE) +
-    theme(legend.position = "none") +
-    xlab("Distribution") +
-    ylab("Uncertainty term") +
-    xlim(0, 100)
+  geom_density_ridges(fill = "skyblue", jittered_points = TRUE) +
+  theme(legend.position = "none") +
+  xlab("Distribution") +
+  ylab("Uncertainty term") +
+  xlim(0, 100)
 
 # save
 ggsave("terms.png", width = 1920, height = 1920, units = "px", dpi = 300)
